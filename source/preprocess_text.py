70303600155c5c15f0bf2105622416f1a82056fc
Sean Jeong <seanj118@gmail.com>
2023-11-28 13:06:48 -0800

    added preprocessing function

---
+++ source/preprocess_text.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+def preprocess(text):
+    # converts to lowercase
+    text = text.lower()
+    
+    # removes puncutation, non-alphabet characters
+    text = text.translate(str.maketrans('','',string.punctuation))
+    text = ''.join(char for char in text if char.isalpha() or char.isspace())
+                   
+    # tokenizes words
+    tokens = word_tokenize(text)
+    
+    # removes stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token not in stop_words]
+    
+    # lemmatizes, stems words into base forms
+    lemmatizer = WordNetLemmatizer()
+    stemmer = PorterStemmer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    tokens = [stemmer.stem(token) for token in tokens]
+    
+    # combines tokens into a sentence again
+    processed_text = ' '.join(tokens)
+    
+    return processed_text
