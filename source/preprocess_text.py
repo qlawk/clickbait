74f88966b7d58ea4ee7f8691208f31280423d517
Sean Jeong <seanj118@gmail.com>
2023-11-28 17:58:16 -0800

    first model testing
    via random forest classification

--- source/preprocess_text.py
+++ source/preprocess_text.py
@@ -4,6 +4,16 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 
+# creating feature for number of words in each headline
+def count_words(text):
+    return len(text.split())
+
+# creating feature for a ratio of unique words to total words in a headline
+def unique_ratio(text):
+    words = text.split()
+    unique_words = len(set(words)) / len(words)
+    return unique_words
+
 def preprocess(text):
     # converts to lowercase
     text = text.lower()
@@ -29,3 +39,15 @@ def preprocess(text):
     processed_text = ' '.join(tokens)
     
     return processed_text
+
+# commonly found words in tabloids joined w/ top 20 base words from buzzfeed, ew
+sensational_words = ['Shocking','Exclusive','Scandal','Secrets','Revealed','Explosive',
+'Bizarre','Sensational','Outrageous','Forbidden','Terrifying','Stunning','Jaw-Dropping',
+'Unbelievable','Extraordinary','Suddenly','Shockingly','Secretly','Allegedly',
+'Dramatically','Mysteriously','Surprisingly','Eerily','Incredibly','Unexpectedly']
+
+sensational_words = ' '.join(sensational_words)
+sensational_words = preprocess(sensational_words)
+sensational_words = sensational_words + 'product gift thing make thatll season new home day realli youll want say star peopl get one first'
+sensational_words = list(sensational_words.split(' '))
+sensational_words
