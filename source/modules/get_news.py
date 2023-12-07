82863e8ec7b6b3dacefaf1f1a18a58bda2498b04
Sean Jeong <seanj118@gmail.com>
2023-12-07 10:25:06 -0800

    cleaned code, formatted folders
    project complete minus readme

--- source/modules/get_news.py
+++ source/modules/get_news.py
@@ -5,11 +5,19 @@ import requests
 
 # get headlines from news source as csv
 def get_headlines(source):
+
+    # endpoint refers to News API url
     api_endpoint = 'https://newsapi.org/v2/everything'
     headers = {'Authorization': f'Bearer {config.news_key}'}
+
+    # sources are news network names
     params = {'sources': source}
 
     response = requests.get(api_endpoint, headers=headers, params=params)
     data = response.json()
+
+    #  normalized json as a dataframe
     df = pd.json_normalize(data['articles'])
+    
+    # saving dataframe as a csv file
     df.to_csv(source + '.csv',index=False)
